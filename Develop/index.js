22b2767974b9fded61e0bd0279b06bbb642ce575
hqayumie <heelaqayumie@gmail.com>
2022-10-17 15:48:40 -0400

    Created questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,68 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
+
+inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+      },
+      {
+        type: "input",
+        message: "Write a short description about your project.",
+        name: "description"
+      },
+      {
+        type: "editor",
+        message: "Enter installation instructions.",
+        name: "install"
+      },
+      {
+        type: "input",
+        message: "Enter usage information.",
+        name: "usage"
+      },
+      {
+        type: "input",
+        message: "How can others contribute to this repo?",
+        name: "contribution"
+      },
+      {
+        type: "list",
+        message: "What licence is relevant to this project?",
+        name: "licence",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+      },
+      {
+        type: "input",
+        message: "What command should be run to install the dependencies?",
+        name: "dependencies",
+        default: "npm i",
+      },
+      {
+        type: "input";
+        message: "what command should be run to run tests?",
+        default: "npm test",
+      },
+      {
+        type: "input",
+        message: "what is your Github username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "what is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "Enter your image url (https://github.com/your-repository/...)",
+        name: "image"
+      }
+    ])
 const questions = [];
 
 // TODO: Create a function to write README file
