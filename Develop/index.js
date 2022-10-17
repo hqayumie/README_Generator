220d35cc4ed384fbb945eaa567a62839eaa17d86
hqayumie <heelaqayumie@gmail.com>
2022-10-17 16:04:20 -0400

    modified questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -5,71 +5,66 @@ const fs = require('fs');
 
 inquirer
     .prompt([
-      {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title"
-      },
-      {
-        type: "input",
-        message: "Write a short description about your project.",
-        name: "description"
-      },
-      {
-        type: "editor",
-        message: "Enter installation instructions.",
-        name: "install"
-      },
-      {
-        type: "input",
-        message: "Enter usage information.",
-        name: "usage"
-      },
-      {
-        type: "input",
-        message: "How can others contribute to this repo?",
-        name: "contribution"
-      },
-      {
-        type: "list",
-        message: "What licence is relevant to this project?",
-        name: "licence",
-        choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
-      },
-      {
-        type: "input",
-        message: "What command should be run to install the dependencies?",
-        name: "dependencies",
-        default: "npm i",
-      },
-      {
-        type: "input";
-        message: "what command should be run to run tests?",
-        default: "npm test",
-      },
-      {
-        type: "input",
-        message: "what is your Github username?",
-        name: "username"
-      },
-      {
-        type: "input",
-        message: "what is your email address?",
-        name: "email"
-      },
-      {
-        type: "input",
-        message: "Enter your image url (https://github.com/your-repository/...)",
-        name: "image"
-      }
+        {
+            type: "input",
+            message: "what is your Github username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "what is your email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Write a short description about your project.",
+            name: "description"
+        },
+        {
+            type: "list",
+            message: "What licence is relevant to this project?",
+            name: "licence",
+            choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+        },
+        {
+            type: "input",
+            message: "What command should be run to install the dependencies?",
+            name: "dependencies",
+            default: "npm i",
+        },
+        {
+            type: "input";
+            message: "What command should be run to run tests?",
+            default: "npm test",
+        },
+        {
+            type: "input",
+            message: "What does the usere need to know about using the repo?",
+            name: "repo-info",
+        },
+        {
+            type: "input",
+            message: "What does the user need to know to contribute to this repo?",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Enter your image url (https://github.com/your-repository/...)",
+            name: "image"
+        }
     ])
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
